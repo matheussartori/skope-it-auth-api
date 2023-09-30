514702d096650c15ad24bb1c3bf6f853c39a429c
Matheus Sartori <ms.sartori@outlook.com.br>
2023-09-30 14:30:10 -0300

    feat: env validation

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,8 +1,13 @@
 import { AppModule } from '@/infra/app.module'
 import { NestFactory } from '@nestjs/core'
+import { EnvService } from './env/env.service'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+
+  const configService = app.get(EnvService)
+  const port = configService.get('PORT')
+
+  await app.listen(port)
 }
 bootstrap()
