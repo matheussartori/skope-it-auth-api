fc58756812f03cdce87da2f5fa1a32ba06c4b12f
Matheus Sartori <ms.sartori@outlook.com.br>
2023-10-01 00:19:33 -0300

    fix: remove unused files

--- test/infra/fake-refresh-token-provider.ts
+++
@@ -1,7 +0,0 @@
-import { RefreshTokenProvider } from '@/infra/refresh-token-provider'
-
-export class FakeRefreshTokenProvider implements RefreshTokenProvider {
-  generate(): Promise<string> {
-    return Promise.resolve('any_refresh_token')
-  }
-}
