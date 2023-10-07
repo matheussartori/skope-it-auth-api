ffd7b4410b937296a91f36fa6f47aaa8da805526
Matheus Sartori <ms.sartori@outlook.com.br>
2023-10-07 14:47:08 -0300

    chore: add support to planetscale

--- src/infra/database/prisma/migrations/20231007174624_create_initial_tables/migration.sql
+++ src/infra/database/prisma/migrations/20231007174624_create_initial_tables/migration.sql
@@ -24,8 +24,6 @@ CREATE TABLE `refresh_tokens` (
     `created_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     UNIQUE INDEX `refresh_tokens_token_key`(`token`),
+    INDEX `refresh_tokens_user_id_idx`(`user_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- AddForeignKey
-ALTER TABLE `refresh_tokens` ADD CONSTRAINT `refresh_tokens_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
